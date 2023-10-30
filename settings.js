1d3d4b64675394cd83efee36d0f474271b3a3646
selowen <133914035+selowen@users.noreply.github.com>
2023-10-30 15:09:00 -0500

    Add files via upload

---
+++ settings.js
@@ -0,0 +1,17 @@
+// LRS Connection
+let scormReporting = false;
+let xAPIReporting = true;
+let kerRef = "tmmsvT_HaWyn4a1ICl8";
+let secretRef = "JiRgN2o7zbrt5vVqj3g";
+let endPointRef = "https://cloud.scorm.com/lrs/WIGWJ7R4FM/sandbox/"
+
+//Reporting Variables
+let objectID = "https://www.testxAPIslowen";
+let objectName = "Video 1 xAPI Test";
+
+// Display labels
+let videoID = "876729966";
+let title = 'xAP Demo Activity for 2023 ASU Data Conference';
+let description = "This is a demo activity for xAPI and collecting information on the user (name, email), video start, video pause, and video complete data.";
+let copyrightText = '&#169; 2023 ASU Data Conference';
+let profilePic = 'media/me.jpg';
